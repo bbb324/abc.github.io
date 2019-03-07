153d728a692f4f0078e726a37079fb524b80b338
谢峻 <jy.xj@alibaba-inc.com>
2019-03-07 14:28:56 +0800

    增加界面框架

---
+++ src/component/App.jsx
@@ -0,0 +1,12 @@
+/**
+ * Created by junxie on 18/5/27.
+ */
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return <div>23</div>;
+    }
+}
+
+export default App;
