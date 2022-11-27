c976ce201fcc5e58f5ac1ddc847157b0fd4352ce
bbb324 <johnigzie@163.com>
2022-11-27 11:11:52 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bbb324.github.io/',
   css: {
     // css预处理器
     preprocessorOptions: {
