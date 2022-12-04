fe27445c9fdada57565d463963f37d3eb3e068f3
bbb324 <johnigzie@163.com>
2022-12-04 11:29:02 +0800

    add text

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,20 +1,24 @@
 import { useEffect, createRef } from 'react';
+import FirstSceen from './FirstSceen';
 const ref = createRef();
-import './home.less'
+import './home.less';
+
+const scrollEvent = () => {
+  window.onscroll = () => {
+    const header = ref.current;
+    if(window.scrollY > 0){
+      if(!header.classList.contains('shadow')){
+          header.classList.add('shadow');
+      }
+    }else{
+      header.classList.remove('shadow');
+    }
+  }
+}
+
 export default function Home() {
     useEffect(() => {
-      
-      window.onscroll = () => {
-        const header = ref.current;
-       
-        if(window.scrollY > 0){
-          if(!header.classList.contains('shadow')){
-              header.classList.add('shadow');
-          }
-        }else{
-          header.classList.remove('shadow');
-        }
-      }
+      scrollEvent();
     }, [])
     return (<div>
       <div className="header" ref={ref}>
@@ -24,6 +28,9 @@ export default function Home() {
         <span>BLOG</span>
       </nav>
     </div>
-    <div className="content"></div>
+    <div className="content">
+      <FirstSceen />
+
+    </div>
     </div>);
   }
