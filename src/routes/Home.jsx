d6f3747eb2282b6e268b979445006054bbf3acc8
bbb324 <johnigzie@163.com>
2022-12-04 10:23:00 +0800

    add style

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,29 @@
+import { useEffect, createRef } from 'react';
+const ref = createRef();
 import './home.less'
 export default function Home() {
-    return (<div className="header">
+    useEffect(() => {
+      
+      window.onscroll = () => {
+        const header = ref.current;
+       
+        if(window.scrollY > 0){
+          if(!header.classList.contains('shadow')){
+              header.classList.add('shadow');
+          }
+        }else{
+          header.classList.remove('shadow');
+        }
+      }
+    }, [])
+    return (<div>
+      <div className="header" ref={ref}>
       <img src={'/sign.png'} />
       <nav className='nav'>
         <span>ABOUT</span>
         <span>BLOG</span>
       </nav>
+    </div>
+    <div className="content"></div>
     </div>);
   }
